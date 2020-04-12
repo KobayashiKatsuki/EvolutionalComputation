0d89a00347068d8bf3d42555d3f7cbe28e790efa
KobayashiKatsuki <lichtbulm@gmail.com>
2020-04-12 20:06:22 +0900

    WIP

---
+++ src/indivisual.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+個体クラス
+
+・個体：　1つまたは複数の染色体から生成される実体
+　　　　　最適化問題の解とは最適な個体として表現される
+"""
+
+import chromosome
+
+class indivisual:
+    
+    def __init__(self):
+        pass
+    
